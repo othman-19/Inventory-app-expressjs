a75f66c2ed84664adda226aa1e05934833c1d88c
othmane <othman-19@hotmail.fr>
2020-10-09 19:12:54 +0100

    define the Item model

---
+++ models/item.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const ItemSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 100,
+  },
+  description: {
+    type: String,
+    required: true,
+    maxlength: 300,
+  },
+  category: {
+    type: Schema.Types.ObjectId,
+    ref: "Category",
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  numberInStock: {
+    type: Number,
+    required: true,
+  },
+});
+// eslint-disable-next-line no-underscore-dangle
+ItemSchema.virtual("url").get(() => `/inv/item/${this._id}`);

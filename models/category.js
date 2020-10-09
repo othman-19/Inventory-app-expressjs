1ab31dab672fddcc0788b758efe1ca793f9bdb0c
othmane <othman-19@hotmail.fr>
2020-10-09 19:04:27 +0100

    add the category model

---
+++ models/category.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const CategorySchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 100,
+  },
+  description: {
+    type: String,
+    required: true,
+    maxlength: 300,
+  },
+});
+
+// eslint-disable-next-line no-underscore-dangle
+CategorySchema.virtual("url").get(() => `/inv/category/${this._id}`);

26fe3bf93b6d9566107af2c4e80bfd7e996c688c
othmane <othman-19@hotmail.fr>
2020-10-10 19:12:27 +0100

    add the category_list to categorieController

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -2,3 +2,20 @@ const async = require("async");
 const validator = require("express-validator");
 const Category = require("../models/category");
 const Item = require("../models/item");
+
+// Display list of all Categories.
+exports.category_list = (req, res, next) => {
+  Category.find({}, "name")
+    .populate("category")
+    .exec((err, categoryList) => {
+      if (err) {
+        return next(err);
+      }
+      // Successful, so render
+      res.render("category_list", {
+        title: "Category List",
+        categoryList,
+      });
+      return true;
+    });
+};

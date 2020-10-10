bd272f16b1311b2e050adbe76e6b68952fa00f8a
othmane <othman-19@hotmail.fr>
2020-10-10 19:57:45 +0100

    add the category_create_post function to categorieController

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -48,7 +48,7 @@ exports.category_detail = (req, res, next) => {
         category: results.category,
         category_items: results.category_items,
       });
-      return true;
+      return next();
     },
   );
 };
@@ -57,3 +57,52 @@ exports.category_detail = (req, res, next) => {
 exports.category_create_get = (req, res, next) => {
   res.render("category_form", { title: "Create category" });
 };
+
+// Handle Genre create on POST.
+exports.category_create_post = [
+  // Validate that the name field is not empty.
+  validator.body("name", "Category name required").trim().isLength({ min: 1 }),
+
+  // Sanitize (escape) the name field.
+  validator.sanitizeBody("name").escape(),
+
+  // Process request after validation and sanitization.
+  (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validator.validationResult(req);
+
+    // Create a genre object with escaped and trimmed data.
+    const category = new Category({ name: req.body.name });
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render the form again with sanitized values/error messages.
+      res.render("category_form", {
+        title: "Create Category",
+        category,
+        errors: errors.array(),
+      });
+    } else {
+      // Data from form is valid.
+      // Check if Category with same name already exists.
+      Category.findOne({ name: req.body.name }).exec((err, foundCategory) => {
+        if (err) {
+          return next(err);
+        }
+        if (foundCategory) {
+          // Category exists, redirect to its detail page.
+          res.redirect(foundCategory.url);
+        } else {
+          category.save((error) => {
+            if (error) {
+              return next(error);
+            }
+            // Category saved. Redirect to category detail page.
+            res.redirect(category.url);
+            return next();
+          });
+        }
+        return next();
+      });
+    }
+  },
+];

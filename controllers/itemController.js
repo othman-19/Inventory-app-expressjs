1301ff0b6d31b0829f76330c0d2563dcfb8f1f6b
othmane <othman-19@hotmail.fr>
2020-10-11 10:58:28 +0100

    add the item_list function

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -3,3 +3,16 @@ const { body, validationResult } = require("express-validator");
 const Item = require("../models/item");
 const Category = require("../models/category");
 
+// Display list of all items.
+exports.item_list = (req, res, next) => {
+  Item.find({}, "name")
+    .populate("category")
+    .exec((err, itemList) => {
+      if (err) {
+        return next(err);
+      }
+      // Successful, so render
+      res.render("item_list", { title: "Item List", itemList });
+      return next();
+    });
+};

759be77284d728a13c7888c28fceda3b93825df9
othmane <othman-19@hotmail.fr>
2020-10-14 17:45:58 +0100

    change the index route to "/inv", set the basic layout with a sidebar

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get("/", (req, res, next) => {
-  res.redirect("/inventory");
+  res.redirect("/inv");
 });
 
 module.exports = router;

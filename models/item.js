0510ec85a0c86d184c394498106359e029925c74
othmane <othman-19@hotmail.fr>
2020-10-15 09:49:22 +0100

    use function notation in Item model virtual property instead of arrow function because of the this keyword context problem

--- models/item.js
+++ models/item.js
@@ -29,6 +29,9 @@ const ItemSchema = new Schema({
   },
 });
 // eslint-disable-next-line no-underscore-dangle
-ItemSchema.virtual("url").get(() => `/inv/item/${this._id}`);
+ItemSchema.virtual("url").get(function () {
+  // eslint-disable-next-line no-underscore-dangle
+  return `/inv/item/${this._id}`;
+});
 
 module.exports = mongoose.model("Item", ItemSchema);
